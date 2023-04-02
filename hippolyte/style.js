5c941913dfe2e74b7683e069247ce08d4b5ab387
ClovisLamalice <65572026+ClovisLamalice@users.noreply.github.com>
2023-04-02 18:10:46 +0200

    Add files via upload

---
+++ hippolyte/style.js
@@ -0,0 +1,12 @@
+function redirect1() {
+    location.href = '../esteban/main_me.html';
+  }
+  
+  function redirect2() {
+    location.href = '../clovis/index.html';
+  }
+  
+  function redirect3() {
+    location.href = 'comm.html';
+  }
+  
